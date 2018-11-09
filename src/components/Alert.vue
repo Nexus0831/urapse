062b7bdf7a77b05ab92351e16412382836383848
Nexus0831 <pride0831@gmail.com>
2018-11-09 11:57:27 +0900

    feat: Alertサーフェスを作成

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="alert">
+    <div class="surface"></div>
   </div>
 </template>
 
@@ -15,11 +16,33 @@ export default class Alert extends Vue {
 
 <style lang="stylus" scoped>
 #alert
+  /*--- style ---*/
   position fixed
   left 0
   top 0
-  background-color rgba(0, 0, 0, 0.5)
+  background-color rgba(0, 0, 0, 0.3)
   width 100%
   height calc(100vh + 20px)
   z-index 100
+  /*--- end ---*/
+
+  /*--- layout ---*/
+  display flex
+  justify-content center
+  align-items center
+  /*--- end ---*/
+
+.surface
+  /*--- style ---*/
+  background-color #424242
+  height 212px
+  max-height: calc(100% - 96px);
+  width 80%
+  max-width 600px
+  box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+    0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+    0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+  border-radius 4px
+  transition all .3s
+  /*--- end --- */
 </style>
