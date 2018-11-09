7b1000d50156a5afc29b724e1a900472652956e9
Nexus0831 <pride0831@gmail.com>
2018-11-09 17:14:56 +0900

    feat: Button.vueを作成

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="text-button"></button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import rippleEffect from '@/functions/ripple';
+
+@Component
+export default class Button extends Vue {
+  @Prop private text!: string;
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
