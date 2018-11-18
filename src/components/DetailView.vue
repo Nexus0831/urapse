1a6632424d6265d03e7c4f432c270a9f4192a5fb
Nexus0831 <pride0831@gmail.com>
2018-11-18 14:07:52 +0900

    feat: Detailページの雛形を作成

---
+++ src/components/DetailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="detail">
+    detail
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DetailView extends Vue {
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
