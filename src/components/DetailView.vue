a9857e2f8a49ae32cb2aa808693136d95cb0082c
Nexus0831 <pride0831@gmail.com>
2018-11-22 21:34:39 +0900

    fix: svg要素の属性viewportをviewBoxに修正

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="detail">
-    detail
+    <svg width="200" height="200" viewBox="0 0 200 200">
+      <circle cx="100" cy="100" r="100" cursor="pointer" fill="#2196f3"/>
+      <text
+        x="50%"
+        y="50%"
+        fill="white"
+        font-size="28"
+        text-anchor="middle"
+        dominant-baseline="central"
+      >
+        sample
+      </text>
+    </svg>
   </div>
 </template>
 
