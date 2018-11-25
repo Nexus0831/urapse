e070fe7b673e1189bcfe484f3d89dbebab9a71f5
Nexus0831 <pride0831@gmail.com>
2018-11-25 14:51:27 +0900

    feat: loginページを作成

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Detail from './views/Detail.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/detail/:id',
       name: 'detail',
       component: Detail,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
   ],
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <button>login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+</style>
