698f63f372c7411122dfdc7acc908646797fa8ba
Nexus0831 <pride0831@gmail.com>
2018-11-25 15:05:33 +0900

    feat: google login を実装

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="login">
-    <button>login</button>
+    <button @click="googleLogin">login</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
-@Component
+@Component({
+  methods: {
+    googleLogin: () => {
+      firebase.auth().languageCode = 'ja';
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        console.log('ok');
+        console.log(result.user);
+      }).catch((error) => {
+        console.log('error');
+        console.log(error)
+      })
+    }
+  }
+})
 export default class Login extends Vue {}
 </script>
 
