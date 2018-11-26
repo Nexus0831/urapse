d9f1807e59551e59a3f6970b4b93ef1f10ddc91a
Nexus0831 <pride0831@gmail.com>
2018-11-26 09:37:54 +0900

    feat: ContainedButtonの雛形を作成

---
+++ src/components/ContainedButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button></button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContainedButton extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+</style>
